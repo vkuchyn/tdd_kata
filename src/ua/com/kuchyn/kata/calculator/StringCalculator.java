61b919c506857103dc5c75810098d307789b0ea2
viktor <kuchin.victor@gmail.com>
2013-08-20 21:28:02 +0300

    return zero on empty string

---
+++ src/ua/com/kuchyn/kata/calculator/StringCalculator.java
@@ -0,0 +1,23 @@
+package ua.com.kuchyn.kata.calculator;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: viktor
+ * Date: 8/20/13
+ * Time: 9:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StringCalculator
+{
+    public int add(String numbers)
+    {
+        if (numbers.isEmpty())
+        {
+            return 0;
+        }
+
+        throw new NotImplementedException();
+    }
+}
